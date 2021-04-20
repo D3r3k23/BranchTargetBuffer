c665166f9187ce2640fbcf210caefb80bc856291
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-04-20 02:24:01 -0700

    using SM

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 
 #include "BTB.hpp"
 
-#include <iostream>
-
 
 const char* TRACE_FILE = "data/trace_sample.txt";
+using SM = Class_SM;
 
 
 int main(int, char*[])
 {
     auto trace = load_trace_file(TRACE_FILE);
     
-    BTB<Class_SM> btb;
+    BTB<SM> btb;
     btb.process_trace(trace);
     btb.print_to_file("output/BTB.csv");
 
