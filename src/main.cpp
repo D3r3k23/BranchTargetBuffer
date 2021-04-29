c634fb610498627c15db93c7b353f5925e1081f6
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-04-29 16:34:39 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,17 @@
 #include "BTB.hpp"
 
 
-const std::string trace_name = "trace_sample";
+const std::string TRACE_NAME = "trace_sample";
 using SM = Class_SM;
 
 
 int main(int, char*[])
 {
-    if (auto trace = load_trace_file(trace_name + ".txt"); !trace.empty())
+    if (auto trace = load_trace_file(TRACE_NAME + ".txt"); !trace.empty())
     {
         BTB<SM> btb;
         btb.process_trace(trace);
-        btb.print_to_file("BTB." + trace_name + "." + SM::string() + ".csv");
+        btb.print_to_file("BTB." + TRACE_NAME + "." + SM::string() + ".csv");
     }
     return 0;
 }
